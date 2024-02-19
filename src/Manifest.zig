821cd569d2e1cb84b10616b8dbf55367d39b8a71
Matt Knight <mattnite@proton.me>
2024-02-19 01:12:33 -0800

    initial commit

---
+++ src/Manifest.zig
@@ -0,0 +1,242 @@
+allocator: Allocator,
+name: []const u8,
+version: std.SemanticVersion,
+dependencies: std.StringArrayHashMap(PackageInfo),
+paths: std.StringArrayHashMap(void),
+
+const Manifest = @This();
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const zon = @import("eggzon");
+
+const log = std.log.scoped(.manifest);
+
+pub const PackageInfo = union(enum) {
+    local: struct {
+        path: []const u8,
+    },
+    remote: struct {
+        url: []const u8,
+        hash: []const u8,
+    },
+
+    pub fn format(
+        info: PackageInfo,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        switch (info) {
+            .local => |local| try writer.print("local: path={s}", .{local.path}),
+            .remote => |remote| try writer.print("remote: url={s} hash={s}", .{ remote.url, remote.hash }),
+        }
+    }
+};
+
+pub fn from_text(allocator: Allocator, text: []const u8) !Manifest {
+    var result = try zon.parseString(allocator, text);
+    defer result.deinit();
+
+    if (result.root != .object)
+        return error.RootIsNotObject;
+
+    const root = result.root.object;
+    const name = root.get("name") orelse return error.ProjectMissingName;
+    if (name != .string)
+        return error.ProjectNameNotString;
+
+    const version = root.get("version") orelse return error.ProjectMissingVersion;
+    if (version != .string)
+        return error.VersionIsNotString;
+
+    const name_copy = try allocator.dupe(u8, name.string);
+    const semver = try std.SemanticVersion.parse(version.string);
+
+    var paths = std.StringArrayHashMap(void).init(allocator);
+    errdefer {
+        for (paths.keys()) |path| allocator.free(path);
+        paths.deinit();
+    }
+
+    const zon_paths = root.get("paths") orelse return error.ProjectMissingPaths;
+    if (zon_paths != .array)
+        return error.ProjectPathsIsNotArray;
+
+    for (zon_paths.array) |path| {
+        if (path != .string)
+            return error.ProjectPathIsNotString;
+        const path_copy = try allocator.dupe(u8, path.string);
+        errdefer allocator.free(path_copy);
+
+        try paths.put(path_copy, {});
+    }
+
+    var dependencies = std.StringArrayHashMap(PackageInfo).init(allocator);
+    errdefer {
+        for (dependencies.keys(), dependencies.values()) |dep_key, info| {
+            allocator.free(dep_key);
+            switch (info) {
+                .local => |local| allocator.free(local.path),
+                .remote => |remote| {
+                    allocator.free(remote.url);
+                    allocator.free(remote.hash);
+                },
+            }
+        }
+        dependencies.deinit();
+    }
+
+    if (root.get("dependencies")) |dependencies_node| blk: {
+        if (dependencies_node == .empty)
+            break :blk;
+        if (dependencies_node != .object)
+            return error.DependenciesIsNotObject;
+
+        for (dependencies_node.object.keys(), dependencies_node.object.values()) |dep_key, dep_value| {
+            if (dep_value != .object)
+                return error.DependencyIsNotObject;
+
+            const dep = dep_value.object;
+            const dep_key_copy = try allocator.dupe(u8, dep_key);
+            errdefer allocator.free(dep_key_copy);
+
+            if (dep.get("path")) |path| {
+                if (path != .string)
+                    return error.DependencyPathIsNotString;
+
+                const path_copy = try allocator.dupe(u8, path.string);
+                errdefer allocator.free(path_copy);
+
+                try dependencies.put(dep_key_copy, .{
+                    .local = .{ .path = path_copy },
+                });
+            } else {
+                const url = dep.get("url") orelse return error.DependencyMissingUrl;
+                const hash = dep.get("hash") orelse return error.DependencyMissingHash;
+
+                if (url != .string)
+                    return error.UrlIsNotString;
+
+                if (hash != .string)
+                    return error.HashIsNotString;
+
+                const url_copy = try allocator.dupe(u8, url.string);
+                errdefer allocator.free(url_copy);
+
+                const hash_copy = try allocator.dupe(u8, hash.string);
+                errdefer allocator.free(url_copy);
+
+                try dependencies.put(dep_key_copy, .{
+                    .remote = .{
+                        .url = url_copy,
+                        .hash = hash_copy,
+                    },
+                });
+            }
+        }
+    }
+
+    return Manifest{
+        .allocator = allocator,
+        .name = name_copy,
+        .version = semver,
+        .dependencies = dependencies,
+        .paths = paths,
+    };
+}
+
+pub fn deinit(manifest: *Manifest) void {
+    manifest.allocator.free(manifest.name);
+
+    for (manifest.dependencies.keys(), manifest.dependencies.values()) |name, info| {
+        manifest.allocator.free(name);
+        switch (info) {
+            .local => |local| manifest.allocator.free(local.path),
+            .remote => |remote| {
+                manifest.allocator.free(remote.url);
+                manifest.allocator.free(remote.hash);
+            },
+        }
+    }
+    manifest.dependencies.deinit();
+
+    for (manifest.paths.keys()) |path| manifest.allocator.free(path);
+    manifest.paths.deinit();
+}
+
+pub fn format(
+    manifest: Manifest,
+    comptime fmt: []const u8,
+    options: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    _ = fmt;
+    _ = options;
+
+    try writer.writeByte('\n');
+    try writer.print("\tname: {s}\n", .{manifest.name});
+    try writer.print("\tversion: {}\n", .{manifest.version});
+    try writer.writeAll("\tpaths:\n");
+    for (manifest.paths.keys()) |path|
+        try writer.print("\t\t{s}\n", .{path});
+
+    if (manifest.dependencies.count() > 0) {
+        try writer.writeAll("\tdependencies:\n");
+        for (manifest.dependencies.keys(), manifest.dependencies.values()) |dep_name, dep|
+            try writer.print("\t\t{s}: {}\n", .{ dep_name, dep });
+    }
+}
+
+pub fn serialize(manifest: Manifest, allocator: Allocator) ![]const u8 {
+    var buffer = std.ArrayList(u8).init(allocator);
+    const writer = buffer.writer();
+
+    try writer.writeAll(".{\n");
+    try writer.print(
+        \\    .name = "{s}",
+        \\    .version = "{}",
+        \\    .paths = .{{
+        \\
+    , .{ manifest.name, manifest.version });
+    for (manifest.paths.keys()) |path|
+        try writer.print(
+            \\        "{s}"
+            \\
+        , .{path});
+    try writer.writeAll(
+        \\    },
+        \\
+    );
+
+    if (manifest.dependencies.count() > 0) {
+        try writer.writeAll(
+            \\    .dependencies = .{
+            \\
+        );
+
+        for (manifest.dependencies.keys(), manifest.dependencies.values()) |dep_name, info| {
+            try writer.print(
+                \\        .{s} = .{{
+                \\            .url = "{s}",
+                \\            .hash = "{s}",
+                \\        }},
+                \\
+            , .{
+                std.zig.fmtId(dep_name),
+                info.remote.url,
+                info.remote.hash,
+            });
+        }
+
+        try writer.writeAll(
+            \\    },
+            \\
+        );
+    }
+    try writer.writeAll("}\n");
+
+    return try buffer.toOwnedSlice();
+}
---
+++ src/main.zig
@@ -0,0 +1,153 @@
+const std = @import("std");
+
+const Manifest = @import("Manifest.zig");
+const Archive = @import("Archive.zig");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+    const args = try std.process.argsAlloc(allocator);
+
+    const base_url = args[1];
+
+    var todo = std.StringArrayHashMap(void).init(allocator);
+    var manifests = std.StringArrayHashMap(Manifest).init(allocator);
+    var dependencies = std.StringArrayHashMap(std.StringArrayHashMapUnmanaged([]const u8)).init(allocator);
+
+    try todo.put(try std.fs.cwd().realpathAlloc(allocator, "."), {});
+    while (todo.count() > 0) {
+        // pop entries from todo until it's ones we haven't visited
+        const root_path = while (todo.popOrNull()) |entry| {
+            if (!manifests.contains(entry.key))
+                break entry.key;
+        } else continue;
+
+        var root_dir = try std.fs.openDirAbsolute(root_path, .{});
+        defer root_dir.close();
+
+        const zon_text = try root_dir.readFileAlloc(allocator, "build.zig.zon", 0x4000);
+        const manifest = try Manifest.from_text(allocator, zon_text);
+        try manifests.putNoClobber(root_path, manifest);
+        std.log.info("created manifest: {s}", .{root_path});
+
+        const result = try dependencies.getOrPut(root_path);
+        std.debug.assert(!result.found_existing);
+
+        result.value_ptr.* = .{};
+
+        for (manifest.dependencies.keys(), manifest.dependencies.values()) |dep_name, dep|
+            switch (dep) {
+                .local => |local| {
+                    const realpath = try root_dir.realpathAlloc(allocator, local.path);
+                    try todo.put(realpath, {});
+                    try result.value_ptr.put(allocator, dep_name, realpath);
+                },
+                .remote => {},
+            };
+    }
+
+    const root_path = try std.fs.cwd().realpathAlloc(allocator, ".");
+    var depths = std.StringArrayHashMap(u32).init(allocator);
+    for (manifests.keys()) |path|
+        try depths.put(path, 0);
+
+    // calculate the depths of all the packages
+    {
+        var changes_made = true;
+        while (changes_made) {
+            changes_made = false;
+            for (depths.keys(), depths.values()) |path, depth| {
+                for (dependencies.get(path).?.values()) |dep_path| {
+                    const dep_depth = depths.get(dep_path).?;
+                    if (dep_depth <= depth) {
+                        try depths.put(dep_path, depth + 1);
+                        changes_made = true;
+                    }
+                }
+            }
+        }
+    }
+
+    var archives = std.StringArrayHashMap(Archive).init(allocator);
+    var hashes = std.StringArrayHashMap([Archive.Hash.digest_length]u8).init(allocator);
+    var urls = std.StringArrayHashMap([]const u8).init(allocator);
+
+    var d: isize = @intCast(std.mem.max(u32, depths.values()));
+    const root_manifest = manifests.get(root_path).?;
+    while (d > -1) : (d -= 1) {
+        for (depths.keys(), depths.values()) |path, depth| {
+            if (std.mem.eql(u8, root_path, path))
+                continue;
+
+            if (d == depth) {
+                const local_deps = dependencies.get(path).?;
+                var manifest = manifests.get(path).?;
+                for (local_deps.keys(), local_deps.values()) |dep_name, dep_path| {
+                    try manifest.dependencies.put(dep_name, .{
+                        .remote = .{
+                            .url = urls.get(dep_path).?,
+                            .hash = try std.fmt.allocPrint(allocator, "{}", .{std.fmt.fmtSliceHexLower(&hashes.get(dep_path).?)}),
+                        },
+                    });
+                }
+
+                var dir = try std.fs.openDirAbsolute(path, .{ .iterate = true });
+                defer dir.close();
+
+                var archive = try Archive.read_from_fs(allocator, dir, manifest.paths);
+                if (archive.files.getPtr("build.zig.zon")) |file|
+                    file.text = try manifest.serialize(allocator);
+                try hashes.put(path, try archive.hash(allocator, .ignore_executable_bit));
+                try archives.put(path, archive);
+                try urls.put(path, try std.fmt.allocPrint(allocator, "{s}/{s}-{}/{s}-{}.tar.gz", .{
+                    base_url,
+                    root_manifest.name,
+                    root_manifest.version,
+                    manifest.name,
+                    manifest.version,
+                }));
+            }
+        }
+    }
+
+    try std.fs.cwd().deleteTree("boxzer-out");
+    var out_dir = try std.fs.cwd().makeOpenPath("boxzer-out", .{});
+    defer out_dir.close();
+
+    for (manifests.keys(), manifests.values()) |path, manifest| {
+        if (std.mem.eql(u8, root_path, path))
+            continue;
+
+        const file = if (std.mem.eql(u8, path, root_path)) blk: {
+            const out_path = try std.fmt.allocPrint(allocator, "{s}-{}.tar.gz", .{ manifest.name, manifest.version });
+            break :blk try out_dir.createFile(out_path, .{});
+        } else blk: {
+            const out_path = try std.fmt.allocPrint(allocator, "{s}-{}/{s}-{}.tar.gz", .{
+                root_manifest.name,
+                root_manifest.version,
+                manifest.name,
+                manifest.version,
+            });
+
+            var dir = try out_dir.makeOpenPath(std.fs.path.dirname(out_path).?, .{});
+            defer dir.close();
+
+            break :blk try dir.createFile(std.fs.path.basename(out_path), .{});
+        };
+        defer file.close();
+
+        std.log.debug("archive path: {s}", .{path});
+        const name = try std.fmt.allocPrint(allocator, "{s}-{}", .{ manifest.name, manifest.version });
+        const tar_gz = try archives.get(path).?.to_tar_gz(allocator, name);
+        var buffered = std.io.bufferedWriter(file.writer());
+        try buffered.writer().writeAll(tar_gz);
+        try buffered.flush();
+
+        std.log.info("{s}: {}", .{ path, manifest });
+    }
+}
