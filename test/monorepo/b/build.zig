6b064832cd8b080251b98c4ceddf11ff63268a6c
Matt Knight <mattnite@proton.me>
2024-04-25 17:33:11 -0700

    cleanup

--- test/monorepo/b/build.zig
+++ test/monorepo/b/build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const c_dep = b.dependency("c", .{});
     _ = b.addModule("b", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/root.zig" },
         .imports = &.{
             .{
                 .name = "c",
