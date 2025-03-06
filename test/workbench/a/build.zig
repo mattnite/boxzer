c65de89c450daa591184a03edc96003086c6eabf
Grazfather <grazfather@gmail.com>
2025-03-06 12:15:04 -0500

    Update all test packages to 0.14.0

--- test/workbench/a/build.zig
+++ test/workbench/a/build.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
     const b_dep = b.dependency("b", .{});
     const exe = b.addExecutable(.{
         .name = "a",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.host,
+        .target = target,
     });
     exe.root_module.addImport("b", b_dep.module("b"));
     b.installArtifact(exe);
