a9f49b0aad76f747a5dc819546a20ca52797d03f
Matt Knight <mattnite@proton.me>
2025-08-22 23:47:11 -0700

    Update to zig 0.15.1

--- build.zig
+++ build.zig
@@ -32,9 +32,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = boxzer_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
